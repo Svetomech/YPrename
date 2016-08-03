c8cbca805c0ab324b5cc161ad97acfaf68e60042
Andrey Belyaev <svetomech@gmail.com>
2016-08-03 22:51:56 +0300

    Initial release

---
+++ YPrename/Program.cs
@@ -0,0 +1,134 @@
+﻿using Svetomech.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace YPrename
+{
+  internal static class Program
+  {
+    private const string renameFilesOldPattern = "YP-*1R-*";
+    private const string renameFilesNewPattern = "*";
+
+    static void Main(string[] args)
+    {
+      Console.Title = Application.ProductName;
+      Console.WriteLine();
+
+      string currentFolderPath = Directory.GetCurrentDirectory();
+
+      var filesToRename = new List<string>(Directory.GetFiles(currentFolderPath, renameFilesOldPattern));
+
+      bool searchAccepted = confirmSearchDialog();
+
+      if (!searchAccepted)
+      {
+        messageLine("Exit.");
+        return;
+      }
+
+      Console.WriteLine();
+      renameFiles(filesToRename);
+
+      using (var watcher = new FileSystemWatcher())
+      {
+        watcher.Path = currentFolderPath;
+        watcher.NotifyFilter = NotifyFilters.FileName;
+
+        watcher.Created += new FileSystemEventHandler(renameFileCreated);
+        watcher.Renamed += new RenamedEventHandler(renameFileMoved);
+
+        watcher.EnableRaisingEvents = true;
+
+        string searchPauseText = "stop";
+        Console.Title = $"{Application.ProductName}: Press 'p' to {searchPauseText} searching for new files.";
+
+        for (;;)
+        {
+          char pressedKey = char.ToLower(Console.ReadKey(true).KeyChar);
+
+          if ('p' != pressedKey)
+          {
+            continue;
+          }
+
+          watcher.EnableRaisingEvents = !watcher.EnableRaisingEvents;
+
+          searchPauseText = (searchPauseText == "stop") ? "continue" : "stop";
+          Console.Title = $"{Application.ProductName}: Press 'p' to {searchPauseText} searching for new files.";
+        }
+      }
+    }
+
+    private static bool confirmSearchDialog()
+    {
+      Console.Write("Start searching for files to rename? [Y/n/?] ");
+
+      return (char.ToLower(Console.ReadKey(true).KeyChar) == 'y');
+    }
+
+    private static void messageLine(string text)
+    {
+      SimpleConsole.Line.ClearCurrent();
+      Console.WriteLine(text);
+      Console.ReadKey(true);
+    }
+
+    private static void renameFiles(List<string> filePaths)
+    {
+      if (0 == filePaths.Count)
+      {
+        return;
+      }
+
+      string filePath = filePaths[filePaths.Count - 1];
+      string fileDirectory = Path.GetDirectoryName(filePath);
+      string fileName = Path.GetFileName(filePath);
+
+      string[] partsToReplace = renameFilesOldPattern.Split('*');
+      string[] partsToReplaceWith = renameFilesNewPattern.Split('*');
+
+      for (int i = 0; i < partsToReplaceWith.Length; ++i)
+      {
+        fileName = fileName.Replace(partsToReplace[i], partsToReplaceWith[i]);
+      }
+      string filePathRenamed = Path.Combine(fileDirectory, fileName);
+
+      if (!File.Exists(filePathRenamed) && !SimpleIO.Path.Equals(filePath, filePathRenamed))
+      {
+        File.Move(filePath, filePathRenamed);
+        Console.WriteLine($"\n{Path.GetFileName(filePath)} -> {fileName}");
+      }
+      else
+      {
+        Console.WriteLine($"\n[X] {Path.GetFileName(filePath)} -> {fileName}");
+      }
+
+      filePaths.Remove(filePath);
+      renameFiles(filePaths);
+    }
+
+    private static void renameFileCreated(object sender, FileSystemEventArgs e)
+    {
+      var file = new FileInfo(e.FullPath);
+
+      if (!file.FitsMask(renameFilesOldPattern))
+      {
+        return;
+      }
+
+      while (file.IsLocked())
+      {
+        Thread.Sleep(1);
+      }
+
+      renameFiles(new List<string> { e.FullPath });
+    }
+
+    private static void renameFileMoved(object sender, FileSystemEventArgs e)
+    {
+      renameFileCreated(sender, e);
+    }
+  }
+}
---
+++ YPrename/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("YPrename")]
+[assembly: AssemblyDescription("A simple utility to rename downloads from yp1.yayponies.no into a more suitable format.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Svetomech")]
+[assembly: AssemblyProduct("YPrename")]
+[assembly: AssemblyCopyright("Copyright © Svetomech TheRainwildsKeeper 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b50c5cdd-967d-4326-8533-cdc6d6040fbc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
