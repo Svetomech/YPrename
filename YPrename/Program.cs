38ef8bac460258184197e4220cac0733b01080e1
Andrey Belyaev <svetomech@gmail.com>
2016-10-18 11:11:05 +0300

    Changed spaces from 2 to 4

--- YPrename/Program.cs
+++ YPrename/Program.cs
@@ -7,128 +7,128 @@ using static Svetomech.Utilities.SimpleConsole;
 
 namespace YPrename
 {
-  internal static class Program
-  {
-    private const string renameFilesOldPattern = "YP-*??-*";
-    private const string renameFilesNewPattern = "*";
-
-    // private static AutoResetEvent autoEvent;
-
-    static void Main(string[] args)
+    internal static class Program
     {
-      Console.Title = Application.ProductName;
+        private const string renameFilesOldPattern = "YP-*??-*";
+        private const string renameFilesNewPattern = "*";
 
-      string currentFolderPath = ((args.Length > 0) && Directory.Exists(args[0])) ? args[0] : Application.StartupPath;
+        // private static AutoResetEvent autoEvent;
 
-      var filesToRename = new List<string>(Directory.GetFiles(currentFolderPath, renameFilesOldPattern));
-
-      renameFiles(filesToRename);
-
-      using (var watcher = new FileSystemWatcher())
-      {
-        watcher.Path = currentFolderPath;
-        watcher.NotifyFilter = NotifyFilters.FileName;
-
-        watcher.Created += new FileSystemEventHandler(renameFileCreated);
-        watcher.Renamed += new RenamedEventHandler(renameFileMoved);
-
-        watcher.EnableRaisingEvents = true;
-
-        Thread.Sleep(Timeout.Infinite); // autoEvent.WaitOne();
-      }
-    }
-
-    private static bool confirmRenameDialog()
-    {
-      Console.Write(" [Y/n/?] ");
-
-      return (char.ToLower(Console.ReadKey().KeyChar) == 'y');
-    }
-
-    private static void renameFiles(List<string> filePaths)
-    {
-      if (0 == filePaths.Count)
-      {
-        Console.Title = Application.ProductName;
-        return;
-      }
-
-      Console.Title = $"{Application.ProductName}: NEW FILE FOUND";
-
-      string filePath = filePaths[filePaths.Count - 1];
-      string fileDirectory = Path.GetDirectoryName(filePath);
-      string fileName = Path.GetFileName(filePath);
-
-      string[] partsToReplace = renameFilesOldPattern.Split('*');
-      string[] partsToReplaceWith = renameFilesNewPattern.Split('*');
-
-      for (int i = 0; i < partsToReplaceWith.Length; ++i)
-      {
-        if (partsToReplace[i].Contains("?"))
+        static void Main(string[] args)
         {
-          // Helper variables, only needed to clarify meaning
-          char lastSignificantCharacter = renameFilesOldPattern[renameFilesOldPattern.LastIndexOf('*') - 1];
-          int questionMarkCount = renameFilesOldPattern.Length - renameFilesOldPattern.Replace("?", "").Length;
+            Console.Title = Application.ProductName;
 
-          // Actual variables
-          int baseDisposition = 0; // ... to keep track of removed question marks
-          int baseIndexOfQM = fileName.LastIndexOf(lastSignificantCharacter) - questionMarkCount; // ... in fileName
-          int indexOfQM = -1; // ... in renameFilesOldPattern
+            string currentFolderPath = ((args.Length > 0) && Directory.Exists(args[0])) ? args[0] : Application.StartupPath;
 
-          // Replaces all question marks with actual characters that represent them
-          while (-1 != (indexOfQM = partsToReplace[i].IndexOf("?")))
-          {
-            partsToReplace[i] = partsToReplace[i].Remove(indexOfQM, 1);
-            partsToReplace[i] = partsToReplace[i].Insert(indexOfQM, fileName[baseIndexOfQM + baseDisposition++].ToString());
-          }
+            var filesToRename = new List<string>(Directory.GetFiles(currentFolderPath, renameFilesOldPattern));
+
+            renameFiles(filesToRename);
+
+            using (var watcher = new FileSystemWatcher())
+            {
+                watcher.Path = currentFolderPath;
+                watcher.NotifyFilter = NotifyFilters.FileName;
+
+                watcher.Created += new FileSystemEventHandler(renameFileCreated);
+                watcher.Renamed += new RenamedEventHandler(renameFileMoved);
+
+                watcher.EnableRaisingEvents = true;
+
+                Thread.Sleep(Timeout.Infinite); // autoEvent.WaitOne();
+            }
         }
 
-        fileName = fileName.Replace(partsToReplace[i], partsToReplaceWith[i]);
-      }
-      string filePathRenamed = Path.Combine(fileDirectory, fileName);
+        private static bool confirmRenameDialog()
+        {
+            Console.Write(" [Y/n/?] ");
 
-      Console.Write($"\n[ ] \"{Path.GetFileName(filePath)}\" -> \"{fileName}\"");
-      bool renamingAccepted = confirmRenameDialog();
-      Line.ClearCurrent();
+            return (char.ToLower(Console.ReadKey().KeyChar) == 'y');
+        }
 
-      if (!renamingAccepted)
-      {
-        Console.WriteLine($"[n] \"{Path.GetFileName(filePath)}\" -> \"{fileName}\"");
-      }
-      else if (File.Exists(filePathRenamed) || SimpleIO.Path.Equals(filePath, filePathRenamed))
-      {
-        Console.WriteLine($"[!] \"{Path.GetFileName(filePath)}\" -> \"{fileName}\"");
-      }
-      else
-      {
-        File.Move(filePath, filePathRenamed);
-        Console.WriteLine($"[y] \"{Path.GetFileName(filePath)}\" -> \"{fileName}\"");
-      }
+        private static void renameFiles(List<string> filePaths)
+        {
+            if (0 == filePaths.Count)
+            {
+                Console.Title = Application.ProductName;
+                return;
+            }
 
-      filePaths.Remove(filePath);
-      renameFiles(filePaths);
+            Console.Title = $"{Application.ProductName}: NEW FILE FOUND";
+
+            string filePath = filePaths[filePaths.Count - 1];
+            string fileDirectory = Path.GetDirectoryName(filePath);
+            string fileName = Path.GetFileName(filePath);
+
+            string[] partsToReplace = renameFilesOldPattern.Split('*');
+            string[] partsToReplaceWith = renameFilesNewPattern.Split('*');
+
+            for (int i = 0; i < partsToReplaceWith.Length; ++i)
+            {
+                if (partsToReplace[i].Contains("?"))
+                {
+                    // Helper variables, only needed to clarify meaning
+                    char lastSignificantCharacter = renameFilesOldPattern[renameFilesOldPattern.LastIndexOf('*') - 1];
+                    int questionMarkCount = renameFilesOldPattern.Length - renameFilesOldPattern.Replace("?", "").Length;
+
+                    // Actual variables
+                    int baseDisposition = 0; // ... to keep track of removed question marks
+                    int baseIndexOfQM = fileName.LastIndexOf(lastSignificantCharacter) - questionMarkCount; // ... in fileName
+                    int indexOfQM = -1; // ... in renameFilesOldPattern
+
+                    // Replaces all question marks with actual characters that represent them
+                    while (-1 != (indexOfQM = partsToReplace[i].IndexOf("?")))
+                    {
+                        partsToReplace[i] = partsToReplace[i].Remove(indexOfQM, 1);
+                        partsToReplace[i] = partsToReplace[i].Insert(indexOfQM, fileName[baseIndexOfQM + baseDisposition++].ToString());
+                    }
+                }
+
+                fileName = fileName.Replace(partsToReplace[i], partsToReplaceWith[i]);
+            }
+            string filePathRenamed = Path.Combine(fileDirectory, fileName);
+
+            Console.Write($"\n[ ] \"{Path.GetFileName(filePath)}\" -> \"{fileName}\"");
+            bool renamingAccepted = confirmRenameDialog();
+            Line.ClearCurrent();
+
+            if (!renamingAccepted)
+            {
+                Console.WriteLine($"[n] \"{Path.GetFileName(filePath)}\" -> \"{fileName}\"");
+            }
+            else if (File.Exists(filePathRenamed) || SimpleIO.Path.Equals(filePath, filePathRenamed))
+            {
+                Console.WriteLine($"[!] \"{Path.GetFileName(filePath)}\" -> \"{fileName}\"");
+            }
+            else
+            {
+                File.Move(filePath, filePathRenamed);
+                Console.WriteLine($"[y] \"{Path.GetFileName(filePath)}\" -> \"{fileName}\"");
+            }
+
+            filePaths.Remove(filePath);
+            renameFiles(filePaths);
+        }
+
+        private static void renameFileCreated(object sender, FileSystemEventArgs e)
+        {
+            var file = new FileInfo(e.FullPath);
+
+            if (!file.FitsMask(renameFilesOldPattern))
+            {
+                return;
+            }
+
+            while (file.IsLocked())
+            {
+                Thread.Sleep(1);
+            }
+
+            renameFiles(new List<string> { e.FullPath });
+        }
+
+        private static void renameFileMoved(object sender, FileSystemEventArgs e)
+        {
+            renameFileCreated(sender, e);
+        }
     }
-
-    private static void renameFileCreated(object sender, FileSystemEventArgs e)
-    {
-      var file = new FileInfo(e.FullPath);
-
-      if (!file.FitsMask(renameFilesOldPattern))
-      {
-        return;
-      }
-
-      while (file.IsLocked())
-      {
-        Thread.Sleep(1);
-      }
-
-      renameFiles(new List<string> { e.FullPath });
-    }
-
-    private static void renameFileMoved(object sender, FileSystemEventArgs e)
-    {
-      renameFileCreated(sender, e);
-    }
-  }
 }
